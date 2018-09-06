4e0127d501cf652b5d4ce6a24fba8afc1a9c54ea
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-09-06 14:18:28 +0200

    Extract the name of the parameter from the method signature
    This commit refactors the rewrite to use SemanticDocument to query the
    name of the parameter of the argument we're calling. The call-site could
    have multiple arguments, so we keep track of the index with
    `zipWithIndex`.

--- scalafix/rules/src/main/scala/fix/Namedliteralarguments_v1.scala
+++ scalafix/rules/src/main/scala/fix/Namedliteralarguments_v1.scala
@@ -9,10 +9,25 @@ class Namedliteralarguments_v1
   override def fix(implicit doc: SemanticDocument): Patch = {
     doc.tree
       .collect {
-        case Term.Apply(_, args) =>
-          args.collect {
-            case t @ Lit.Boolean(_) =>
-              Patch.addLeft(t, "isSuccess = ")
+        case Term.Apply(fun, args) =>
+          args.zipWithIndex.collect {
+            case (t @ Lit.Boolean(_), i) =>
+              fun.symbol.info match {
+                case Some(info) =>
+                  info.signature match {
+                    case method: MethodSignature
+                        if method.parameterLists.nonEmpty =>
+                      val parameter = method.parameterLists.head(i)
+                      val parameterName = parameter.displayName
+                      Patch.addLeft(t, s"$parameterName = ")
+                    case _ =>
+                      // Do nothing, the symbol is not a method
+                      Patch.empty
+                  }
+                case None =>
+                  // Do nothing, we don't have information about this symbol.
+                  Patch.empty
+              }
           }
       }
       .flatten
