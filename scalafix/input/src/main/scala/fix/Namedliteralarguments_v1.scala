0cec867afcc39da74e48e83250b91ed8db3d5afb
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-09-06 14:18:28 +0200

    Extract the name of the parameter from the method signature
    This commit refactors the rewrite to use SemanticDocument to query the
    name of the parameter of the argument we're calling. The call-site could
    have multiple arguments, so we keep track of the index with
    `zipWithIndex`.

--- scalafix/input/src/main/scala/fix/Namedliteralarguments_v1.scala
+++ scalafix/input/src/main/scala/fix/Namedliteralarguments_v1.scala
@@ -5,9 +5,9 @@ package fix
 
 object Namedliteralarguments_v1_Test {
   def complete(isSuccess: Boolean): Unit = ()
-  def finish(isError: Boolean): Unit = ()
+  def finish(n: Int, isError: Boolean): Unit = ()
   complete(true)
   complete(isSuccess = true)
   complete(false)
-  finish(true)
+  finish(2, true)
 }
