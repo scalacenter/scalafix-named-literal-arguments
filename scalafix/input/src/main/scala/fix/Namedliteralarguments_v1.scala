dae1be330e72ccf94f31e54eb5353cf26a300fd8
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-09-06 14:07:58 +0200

    Reproduce buggy behavior
    We have hardcoded the name `isSemantic`, which means that the rewrite
    produces buggy code when the parameter has a different name.  The tests
    fail with the diff
    
    ```diff
    --- obtained
    +++ expected
    @@ -8,3 +8,3 @@
       complete(isSuccess = false)
    -  finish(isSuccess = true)
    +  finish(isError = true)
     }
    ```

--- scalafix/input/src/main/scala/fix/Namedliteralarguments_v1.scala
+++ scalafix/input/src/main/scala/fix/Namedliteralarguments_v1.scala
@@ -5,7 +5,9 @@ package fix
 
 object Namedliteralarguments_v1_Test {
   def complete(isSuccess: Boolean): Unit = ()
+  def finish(isError: Boolean): Unit = ()
   complete(true)
   complete(isSuccess = true)
   complete(false)
+  finish(true)
 }
--- scalafix/output/src/main/scala/fix/Namedliteralarguments_v1.scala
+++ scalafix/output/src/main/scala/fix/Namedliteralarguments_v1.scala
@@ -2,7 +2,9 @@ package fix
 
 object Namedliteralarguments_v1_Test {
   def complete(isSuccess: Boolean): Unit = ()
+  def finish(isError: Boolean): Unit = ()
   complete(isSuccess = true)
   complete(isSuccess = true)
   complete(isSuccess = false)
+  finish(isError = true)
 }
