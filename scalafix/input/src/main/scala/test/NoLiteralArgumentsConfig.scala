1a8cafdc77987926b7cdbde88419bceef7beef51
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-09-07 17:36:40 +0200

    Separate NoLiteralArguments into a different file

---
+++ scalafix/input/src/main/scala/test/NoLiteralArgumentsConfig.scala
@@ -0,0 +1,20 @@
+/*
+rule = NoLiteralArguments
+NoLiteralArguments.disabledLiterals = [
+  Int
+  String
+  Boolean
+]
+ */
+package test
+
+class NoLiteralArgumentsConfig {
+  def complete(message: String): Unit = ()
+  def complete(count: Int): Unit = ()
+  def complete(isSuccess: Boolean): Unit = ()
+  def complete(code: Char): Unit = ()
+  complete("done") // assert: NoLiteralArguments
+  complete(42) // assert: NoLiteralArguments
+  complete(true) // assert: NoLiteralArguments
+  complete('o')
+}
